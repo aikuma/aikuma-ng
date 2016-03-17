ee9718a1da83a2b9f74a4378bb194fe6c6a80cac
Mat Bettinson <mat@plothatching.com>
2016-03-17 17:36:33 +1100

    kinda sorta annotation :)

--- src/annoweb-annotation.js
+++ src/annoweb-annotation.js
@@ -29,7 +29,8 @@
         var rwKeyCode = 37;     // left arrow
         var escKeyCode = 27;    // escape
         var ffPlaybackRate = 2.5; // playback speed in FF mode
-        var skipTimeValue = 1;  // amount of time to skip backwards for rewind
+        var skipTimeValue = 3;  // amount of time to skip backwards for rewind
+        var oneMillisecond = 0.001;
         vm.selectedAnno = 0;
         vm.annoEnabled = {};
         vm.curRegion = 0;
@@ -82,16 +83,20 @@
             keyService.regKey(rwKeyCode,'keydown',  function(ev)  {rwKey(true);});
             keyService.regKey(escKeyCode,'keydown', function(ev)  {escKey(true);});
             setWsRegions(0);
+            vm.playIn = _.last(vm.regionList).end;
             $scope.$broadcast('inputfoo0');
         });
         wsAnnotate.on('region-in', function(reg) {
             vm.curRegion = reg.data.idx;
-            console.log(reg.data.idx);
+            $scope.$apply();
+        });
+        wsAnnotate.on('region-out', function(reg) {
+            vm.curRegion = null;
             $scope.$apply();
         });
         wsAnnotate.on('audioprocess', function() {
             var currentPos = wsAnnotate.getCurrentTime();
-            if (vm.isPlaying) {
+            if (vm.regionMarked) {
                 _.last(vm.regionList).update({end: currentPos});
             }
         });
@@ -104,9 +109,23 @@
             if (vm.ffKeyDown) {return;}  // Block multiple keys
             vm.playKeyDown = true;
             vm.isPlaying = true;
-            var thisTime = wsAnnotate.getCurrentTime();
-            makeNewRegion(thisTime);
-            wsAnnotate.play();
+
+            if (vm.regionMarked) {
+                wsAnnotate.play(vm.playIn);
+            } else {
+                var thisTime = wsAnnotate.getCurrentTime();
+                if (thisTime >= vm.playIn) {
+                    makeNewRegion(thisTime);
+                    vm.regionMarked = true;
+                    console.log(vm.regionList.length);
+                    vm.curRegion = vm.regionList.length - 1;
+                    vm.playIn = thisTime;
+                    console.log(vm.curRegion);
+                }
+                wsAnnotate.play();
+            }
+            if (nokey) {$scope.$apply();}
+
         }
         vm.playDown = function() {
             playKeyDown(true);
@@ -123,21 +142,60 @@
         function ffKeyDown(nokey) {
             if (vm.playKeyDown) {return;}  // Block multiple keys
             vm.ffKeyDown = true;
-            if (vm.regionMarked) {deleteLastRegion();}
-            wsAnnotate.setPlaybackRate(ffPlaybackRate);
-            wsAnnotate.play();
+            var thisTime = wsAnnotate.getCurrentTime();
+            if (thisTime < vm.playIn) {
+                var seeked = false;
+                vm.regionList.every(function(reg, index) {
+                    if (reg.start > thisTime) {
+                        seekToTime(reg.start);
+                        vm.curRegion = index;
+                        seeked = true;
+                        return false;
+                    } else {return true;}
+                });
+                if (!seeked) {
+                    vm.curRegion = null;
+                    seekToTime(vm.playIn+oneMillisecond);
+                    seekToPlayin();
+                }
+
+            } else {
+                if (vm.regionMarked) {
+                    deleteLastRegion();
+                }
+                wsAnnotate.setPlaybackRate(ffPlaybackRate);
+                wsAnnotate.play();
+            }
+            if (nokey) {$scope.$apply();}
         }
 
         function ffKeyUp(nokey) {
             vm.ffKeyDown = false;
             if (vm.playKeyDown) {return;}  // Block multiple keys
-            wsAnnotate.pause();
-            wsAnnotate.setPlaybackRate(1);
+            if (wsAnnotate.isPlaying()) {
+                wsAnnotate.pause();
+                wsAnnotate.setPlaybackRate(1);
+            }
         }
 
         function rwKey(nokey) {
-            wsAnnotate.skipBackward(skipTimeValue);
             if (vm.regionMarked) {deleteLastRegion();}
+            var thisTime = wsAnnotate.getCurrentTime();
+            if ((thisTime - skipTimeValue) < vm.playIn) {
+                if (vm.curRegion != 0) {
+                    var lastidx = _.findLastIndex(vm.regionList, function (reg) {
+                        if (reg.start < thisTime) {
+                            seekToTime(reg.start);
+                            return true;
+                        }
+                    });
+                    vm.curRegion = lastidx;
+                } else {
+                    seekToTime(vm.regionList[0].start);
+                }
+            } else {
+                wsAnnotate.skipBackward(skipTimeValue);
+            }
             if (nokey) {$scope.$apply();}
         }
 
@@ -176,7 +234,7 @@
                 data: col
             });
             vm.regionList.push(reg);
-            vm.regionMarked = true;
+
         }
 
         // delete the last audio, remove the wavesurfer region, seek to playIn, disable recording and make a new Segmap
@@ -204,17 +262,31 @@
             );
         }
 
+        // Often we force seek to play-in because we don't allow the user to play back where they cannot record.
+        function seekToPlayin() {
+            var length = wsAnnotate.getDuration();
+            var floatpos = vm.playIn / length;
+            wsAnnotate.seekTo(floatpos);
+        }
+        function seekToTime(time) {
+            var length = wsAnnotate.getDuration();
+            var floatpos = time / length;
+            wsAnnotate.seekTo(floatpos);
+        }
+
         function setWsRegions(annoIdx) {
             wsAnnotate.clearRegions();
             var sm = aikumaService.getSegmap(vm.annotations[annoIdx].SegId);
-            var aidx = 0;
+            var aix = 0;
             vm.annotations[annoIdx].annos.forEach(function(an){
-                var stime = sm[aidx].source[0] / 1000;
-                var etime = sm[aidx].source[1] / 1000;
+                var stime = sm[aix].source[0] / 1000;
+                var etime = sm[aix].source[1] / 1000;
                 makeNewRegion(stime);
                 _.last(vm.regionList).update({end:etime});
                 markLastRegionComplete();
+                ++aix;
             });
+
         }
     };
 
