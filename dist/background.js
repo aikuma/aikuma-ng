6715a84d184fbd093a2e143ad881b4484a3c8405
lingomat <mat@plothatching.com>
2016-04-07 23:57:06 +0800

    Remove files from repo

--- dist/background.js
+++
@@ -1,6 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('window.html', {
-    'outerBounds': {width: 1280, height: 768},
-    'state': 'maximized'
-  });
-});
