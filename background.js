96739aedde4fcb3b609efc3d5bced7edebbda2cf
lingomat <mat@plothatching.com>
2016-04-08 02:41:12 +0800

    Responsive layout improvements.

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('window.html', {
-    'outerBounds': {width: 1280, height: 768},
+    'outerBounds': {width: 1280, height: 768, minWidth: 960},
     'state': 'maximized'
   });
 });
