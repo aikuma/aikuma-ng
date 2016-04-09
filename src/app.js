58259d3ea5a2cdf2130eb7a43172fd47076cad73
lingomat <mat@plothatching.com>
2016-04-09 23:38:45 +0800

    Chinese localisation.

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@
         ])
         .constant('config', {
             appName: 'AikumaNG',
-            appVersion: '0.910',
+            appVersion: '0.911',
             sampleRate: 16000,
             fileStorageMB: 1000,
             debug: false,
